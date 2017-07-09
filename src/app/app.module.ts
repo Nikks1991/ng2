02ac5a2c74a2a7d7c9aa84d64ac5740d795dfd6b
Rohit Ingle <ingle.rohit@gmail.com>
2017-07-09 13:49:43 +0530

    Basic routing functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
+import { Routes, RouterModule } from "@angular/router";
 
-import { MyApp } from './app.component';
-import { Child } from './child.component';
+import { MyApp } from "./app.component";
+import { Child } from "./child.component";
+import { RootComponent } from "./root/root.component";
+import { AboutUsComponent } from "./about-us/about-us.component";
+import { DefaultComponent } from "./default/default.component";
+
+const routes: Routes = [
+  { path: "", component: DefaultComponent },
+  { path: "channel", component: MyApp },
+  { path: "aboutus", component: AboutUsComponent }
+];
 
 @NgModule({
   declarations: [
-    MyApp, Child
+    MyApp,
+    Child,
+    RootComponent,
+    AboutUsComponent,
+    DefaultComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
-  bootstrap: [MyApp]
+  bootstrap: [RootComponent]
 })
-export class AppModule { }
+export class AppModule {}
